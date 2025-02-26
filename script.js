09bffe1705b884b4e8125a740b9adb9b00de2d14
Pretty-blue <nezko.102@gmail.com>
2025-02-26 18:55:48 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yes-btn").addEventListener("click", function() {
+    document.getElementById("details-box").style.display = "block";
+});
+
+document.getElementById("no-btn").addEventListener("click", function() {
+    document.getElementById("details-box").style.display = "none";
+});
